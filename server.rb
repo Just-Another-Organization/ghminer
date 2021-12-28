f2b44fdfa08a226c2df170a3ccde156ca9afb4f1
zappaboy <federico_zappone@hotmail.it>
2021-12-28 17:15:15 +0100

    Added miner configurations

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ require 'mongoid'
 
 CONFIG_BASE_PATH = File.join(File.dirname(__FILE__), 'config')
 MONGOID_CONFIG_PATH = File.join(CONFIG_BASE_PATH, 'mongoid.yml')
+MINER_CONFIG_PATH = File.join(CONFIG_BASE_PATH, 'miner.yml')
 
 before do
   content_type :json
@@ -21,13 +22,18 @@ end
 
 puts 'JA-GHMiner starting'
 Mongoid.load!(MONGOID_CONFIG_PATH)
+miner = Miner.new(MINER_CONFIG_PATH)
+miner.mine
+query = {}
+result_limit = 3
+result = miner.query(query, result_limit)
+puts result[0]
+puts result
 
 get '/health' do
   halt 200, { status: 'Alive' }.to_json
 end
 
 get '/test' do
-  miner = Miner.new
-  miner.mine
   halt 200, { result: 'OK' }.to_json
 end
--- src/miner.rb
+++ src/miner.rb
@@ -2,26 +2,66 @@
 
 require 'gh-archive'
 require 'mongoid'
-
+require 'yaml'
 
 class Commit
   include Mongoid::Document
-  field :author, type: String
+  field :sha, type: String
+  field :author do
+    field :email, type: String
+    field :name, type: String
+  end
+  field :message, type: String
+  field :distinct, type: Boolean
+  field :url, type: String
 end
 
 class Miner
-  def initialize
+  def initialize(config_path = '')
     @provider = GHArchive::OnlineProvider.new
     @provider.include(type: 'PushEvent')
     @provider.exclude(payload: nil)
+
+    if File.file?(config_path)
+      puts "Loading configurations: #{config_path}"
+      @config = YAML.load_file(config_path)['miner']
+    else
+      puts 'Config file not found, using default configurations'
+    end
+
+    @starting_timestamp = @config['starting_timestamp'] || Time.now - 3600 # last hour
+    @ending_timestamp = @config['ending_timestamp'] || Time.now
+    @continuously_updated = @config['continuously_updated'] || false
+    @max_dimension = @config['max_dimension'] || 0
+
+    print_configs
     puts 'Miner ready!'
   end
 
   def mine
     puts 'Mining....'
-    @provider.each(Time.gm(2021, 1, 1, 0, 0, 0), Time.gm(2021, 1, 1, 2, 0, 0)) do |event|
-      event['payload']['commits'].map { |c| Commit.create({ author: c['author']['name'] }) }
+    @provider.each(Time.at(@starting_timestamp), Time.at(@ending_timestamp)) do |event|
+      puts event
+      break
     end
     puts 'Mining finish!!!'
   end
+
+  def query(query, result_limit = 0)
+    puts 'Querying....'
+    Commit.where(query).limit(result_limit)
+  end
+
+  def print_configs
+    puts %{
+##### BEGIN CONFIG #####
+starting_timestamp: #{@starting_timestamp}
+ending_timestamp: #{@ending_timestamp}
+continuously_updated: #{@continuously_updated}
+max_dimension: #{@max_dimension}
+##### END CONFIG #####
+
+}
+  end
+
 end
